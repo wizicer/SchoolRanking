985c2f605db25437ca187897113bfe6bd07dfb5f
LiangShuang <liangshuang@sutu.tech>
2021-02-04 09:09:24 +0800

    change order, add one more data only output

--- Program.cs
+++ Program.cs
@@ -38,15 +38,16 @@
                 .ToArray();
             var jsonColleges = colleges
                 .Select(_ => new CollegeJson(_.Name,
-                    _.Is211 ? "211" : null,
                     _.Is985 ? "985" : null,
+                    _.Is211 ? "211" : null,
+                    _.Ranking == null ? null : $"RNK{_.Ranking}",
                     _.Index == -1 ? null : $"IDX{_.Index}",
-                    //_.Location,
-                    _.Ranking == null ? null : $"RNK{_.Ranking}"
+                    _.Location
                     ))
                 .Select(_ => _ with { Tags = _.Tags.Where(t => t != null).ToArray() })
                 .ToDictionary(_ => _.Name, _ => _.Tags);
 
+            var t = $"var colleges = {JsonConvert.SerializeObject(jsonColleges)};" ;
             var s = $"var colleges = JSON.parse('{JsonConvert.SerializeObject(jsonColleges)}');" + @"
 $('.top-message-menu.iboss-xiaoxitongzhi').after('<a href=""javascript: void(0)"" id=""markcol"">🈯</a>');
 let refreshColTags = function(){
